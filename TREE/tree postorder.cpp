70caee6538efb07caead325b2d0e837b4137c2f0
Sourav kr Chhetri <chettrisourav28@gmail.com>
2020-01-26 21:55:35 +0000

    post order traversal

---
+++ TREE/tree postorder.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+class tree{
+    public:
+    int data;
+    tree*left;
+    tree*right;
+    tree(int d){
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+tree*build(){
+    int data;
+    cin>>data;
+    if(data==-1){
+        return NULL;
+    }
+    tree*root=new tree(data);
+    root->left=build();
+    root->right=build();
+    return root;
+}
+void printpre(tree *root){
+    if(root==NULL){
+        return;
+    }
+    print(root->left);
+    print(root->right);
+    cout<<root->data<<" ";
+}
+int main(){
+    tree*root=build();
+    print(root);
+    return;
+}

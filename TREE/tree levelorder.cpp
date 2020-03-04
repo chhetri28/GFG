26840d160b313f107eb0dd18fee25846ecc9e8c4
chhetri28 <chettrisourav28@gmail.com>
2020-03-05 02:13:25 +0530

    revised

--- TREE/tree levelorder.cpp
+++ TREE/tree levelorder.cpp
@@ -1,56 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
-class tree{
+class node{
     public:
     int data;
-    tree*left;
-    tree*right;
-    tree(int d){
+    node* left;
+    node* right;
+    node(int d){
         data=d;
         left=NULL;
         right=NULL;
     }
 };
-tree*build(){
-    int data;
-    cin>>data;
-    if(data==-1){
+node* buildTree(){
+    int d;
+    cin>>d;
+    if(d==-1){
         return NULL;
     }
-    tree*root=new tree(data);
-    root->left=build();
-    root->right=build();
+    node*root=new node(d);
+    root->left=buildTree();
+    root->right=buildTree();
     return root;
 }
-int height(tree*root){
+int height(node*root){
     if(root==NULL){
         return 0;
     }
     int ls=height(root->left);
     int rs=height(root->right);
-    return max(ls,rs);
+    return max(ls,rs)+1;
 }
-void printknode(tree*node,int k){
-    if(tree==NULL){
+void printkthlevel(node*root,int k){
+    if(root==NULL){
         return;
     }
     if(k==1){
         cout<<root->data<<" ";
         return;
     }
-    printknode(tree*node,k-1);
-    printknode(tree*node,k-1);
-    return;
+        printkthlevel(root->left,k-1);
+        printkthlevel(root->right,k-1);
+        return;
 }
-void levelorder(tree*root){
+void levelorder(node*root){
     int H=height(root);
     for(int i=1;i<=H;i++){
-        printknode(root,i);
+        printkthlevel(root,i);
+        cout<<endl;
     }
     return;
 }
 int main(){
-    tree*root=build();
-    print(root);
-    return;
+    node*root=buildTree();
+    //cout<<height(root);
+    levelorder(root);
 }
+//8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1

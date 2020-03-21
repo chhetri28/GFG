7ab1774d2602e063ae9aafa370cbb53aab3bdbab
chhetri28 <chettrisourav28@gmail.com>
2020-03-22 02:08:11 +0530

    right side of the tree

--- TREE/rightview.cpp
+++ TREE/rightview.cpp
@@ -7,16 +7,32 @@ class node{
     node* right;
     node(int d):data(d),left(NULL),right(NULL){}
 };
-node* build(){
-    int d;
-    cin>>d;
-    if(d==-1){
-        return NULL;
-    }
-    node*root=new node(d);
-    root->left=build();
-    root->right=build();
-    return root;
+node* buildTreeLevelWise(){
+
+	int d;
+	cin>>d;
+
+	node*root = new node(d);
+	queue<node*> q;
+	q.push(root);
+
+	while(!q.empty()){
+
+		node*f = q.front();
+		q.pop();
+		int c1,c2;
+		cin>>c1>>c2;
+
+		if(c1!=-1){
+			f->left = new node(c1);
+			q.push(f->left);
+		}
+		if(c2!=-1){
+			f->right = new node(c2);
+			q.push(f->right);
+		}
+	}
+	return root;
 }
 void helper(node*root,int level,int *max_level){
     if(!root)return;

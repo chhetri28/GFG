1a24025570da86f85c1cda8022e668f4f3ee751a
chhetri28 <chettrisourav28@gmail.com>
2020-03-29 21:30:15 +0530

    bfs

--- TREE/bfs.cpp
+++ TREE/bfs.cpp
@@ -23,33 +23,33 @@ node* buildTree(){
     return root;
 }
 
-// void bfs(node* root){
-//     queue<node*>q;
-//     q.push(root);
-//     q.push(NULL);
-//     while (!q.empty())
-//     {
-//         node* f=q.front();
-//         if(f==NULL){
-//             cout<<endl;
-//             q.pop();
-//             if(!q.empty()){
-//                 q.push(NULL);
-//             }
-//         }
-//         else{
-//             cout<<f->data<<" ";
-//             q.pop();
-//             if(f->left){
-//                 q.push(f->left);
-//             }
-//             if(f->right){
-//                 q.push(f->right);
-//             }
-//         }
-//     }
-//     return;
-// }
+void bfs(node* root){
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node* f=q.front();
+        if(f==NULL){
+            cout<<endl;
+            q.pop();
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<f->data<<" ";
+            q.pop();
+            if(f->left){
+                q.push(f->left);
+            }
+            if(f->right){
+                q.push(f->right);
+            }
+        }
+    }
+    return;
+}
 
 void bfs2(node* root){
     if(root==NULL) return;

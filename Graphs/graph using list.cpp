9c292e0f1ad7ca9fee08d56f8b7c3fe5a5bd917a
chhetri28 <chettrisourav28@gmail.com>
2020-05-19 12:26:31 +0530

    Graph using list

---
+++ Graphs/graph using list.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void addedge(list<int>l[],int u,int v){
+    l[u].push_back(v);
+    l[v].push_back(u);
+}
+void print(list<int>l[],int Ver){
+    for(int v=0;v<Ver;v++){
+        cout<<v;
+        for(int i:l[v]){
+            cout<<"->"<<i;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int Ver;
+    cin>>Ver;
+    list<int>l[Ver];
+    addedge(l,0,1);
+    print(l,Ver);
+}
